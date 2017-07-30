669aa38d00e133a503b61e36d903986dd60f9288
Jared Forsyth <jared@jaredforsyth.com>
2017-07-29 20:38:54 -0600

    load on the fly

--- source.js
+++ source.js
@@ -8,6 +8,7 @@ const buffers = {}
 
 const sources = {}
 const nodes = {}
+const loading = {}
 
 let start = null
 let timer = null
@@ -30,6 +31,7 @@ const play = part => {
     timer = setInterval(() => {
       $('#time').textContent = parseInt((Date.now() - start) / 1000)
     }, 200)
+    $('#time').textContent = parseInt((Date.now() - start) / 1000)
   }
 
   nodes[part].classList.add('playing')
@@ -43,6 +45,38 @@ const play = part => {
   source.start(0, off % buffers[part].duration)
 }
 
+const wait = num => new Promise(r => setTimeout(r, num))
+
+const load = part => fetch(`./mp3s/munamux ${part}.mp3`)
+  // This is to test loading latency
+  // .then(res => wait(2 * 1000).then(() => res))
+  .then(res => res.arrayBuffer())
+  .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+  .then(buffer => buffers[part] = buffer)
+
+const makeButtons = () => {
+  stuff.forEach(part => {
+    const node = document.createElement('button')
+    nodes[part] = node
+    node.className = 'button'
+    node.onmousedown = () => {
+      if (buffers[part]) return play(part)
+      if (loading[part]) return // maybe cancel it?
+      loading[part] = true
+      load(part).then(() => play(part))
+    }
+    node.textContent = part
+    $('#root').appendChild(node)
+  })
+}
+
+// TODO maybe a global play / pause button
+
+makeButtons()
+
+
+
+/*
 Promise.all(stuff.map(part => fetch(`./mp3s/munamux ${part}.mp3`)
   .then(res => res.arrayBuffer())
   .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
@@ -59,4 +93,5 @@ Promise.all(stuff.map(part => fetch(`./mp3s/munamux ${part}.mp3`)
     $('#root').appendChild(node)
   })
 })
+*/
 
